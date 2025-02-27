4f1983b5afaff9d52241c2e050267b04474a4eba
Ahmad shamsedine <ahmadshamsedine@gmail.com>
2025-02-27 11:22:53 +0200

    Loop to add word +1 if exist

--- dictionaries-journey.py
+++ dictionaries-journey.py
@@ -15,4 +15,10 @@ print(result)
 
 def word_count(sentence):
     words = sentence.split()
-    
+    word_dict = {}
+    for word in words:
+         if word in word_dict:
+            word_dict[word] += 1
+         else:
+            word_dict[word] = 1
+    return word_dict    

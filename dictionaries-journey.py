89865b9ce6ffeddb8a79f2cdfc9a5b33e60a00ff
Ahmad shamsedine <ahmadshamsedine@gmail.com>
2025-02-28 09:55:20 +0200

    Print the output_dict

--- dictionaries-journey.py
+++ dictionaries-journey.py
@@ -63,5 +63,7 @@ def invert_dict(input_dict):
             output_dict[value] = [key]
  return output_dict
 
- input_dict = {"Alice": 10, "Bob": 20, "Charlie": 10, "David": 30}
+input_dict = {"Alice": 10, "Bob": 20, "Charlie": 10, "David": 30}
+output_dict = invert_dict(input_dict)
+print(output_dict)
  
